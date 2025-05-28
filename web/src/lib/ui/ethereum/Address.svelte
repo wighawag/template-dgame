8ea9e3cd81ba2da65fb71aee21117182c035bf38
wighawag <wighawag@gmail.com>
2025-05-28 07:07:13 +0100

    feat: implement wait utility functions for asynchronous operations + improve address component

--- web/src/lib/ui/ethereum/Address.svelte
+++ web/src/lib/ui/ethereum/Address.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { wait } from '$lib/utils/time';
 	import { onMount } from 'svelte';
 	import { type HTMLAttributes } from 'svelte/elements';
 	import { createPublicClient, http } from 'viem';
@@ -31,6 +32,7 @@
 		}
 		loading = true;
 		ensName = null;
+		// await wait(2);
 		try {
 			ensName = await publicClient.getEnsName({ address: value });
 		} catch (e) {
@@ -45,30 +47,21 @@
 	}
 </script>
 
-<span {...restProps}>
-	{#if loading}
-		<span class="spinner" aria-label="Loading ENS name..."></span>
-	{:else if ensName}
-		{ensName}
-	{:else}
-		{formatAddress(value)}
-	{/if}
+<span {...restProps} class="inline-flex w-full min-w-[8em] items-center">
+	<span class="flex-1"></span>
+	<span class="flex-0 text-center">
+		{#if ensName}
+			{ensName}
+		{:else}
+			{formatAddress(value)}
+		{/if}
+	</span>
+	<span class="flex flex-1 justify-end">
+		{#if loading}
+			<span
+				class="inline-block h-4 w-4 animate-spin rounded-full border-2 border-gray-300 border-t-gray-800 align-middle"
+				aria-label="Loading ENS name..."
+			></span>
+		{/if}
+	</span>
 </span>
-
-<style>
-	.spinner {
-		display: inline-block;
-		width: 1em;
-		height: 1em;
-		border: 2px solid #ccc;
-		border-top: 2px solid #333;
-		border-radius: 50%;
-		animation: spin 0.8s linear infinite;
-		vertical-align: middle;
-	}
-	@keyframes spin {
-		to {
-			transform: rotate(360deg);
-		}
-	}
-</style>
