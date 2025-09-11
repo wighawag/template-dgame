8d19df9fce1c5f66b70fe389b5ceb4fa642e5bf1
wighawag <wighawag@gmail.com>
2025-09-11 07:46:51 +0100

    fix

--- onchain/evm/test/utils/index.ts
+++ onchain/evm/test/utils/index.ts
@@ -1,6 +1,6 @@
-import {Abi_Avatars} from '#generated/types/Avatars.js';
-import {Abi_AvatarsSale} from '#generated/types/AvatarsSale.js';
-import {Abi_IGame} from '#generated/types/IGame.js';
+import {Abi_Avatars} from '#generated/abis/Avatars.js';
+import {Abi_AvatarsSale} from '#generated/abis/AvatarsSale.js';
+import {Abi_IGame} from '#generated/abis/IGame.js';
 import {loadAndExecuteDeployments} from '#rocketh';
 import {EthereumProvider} from 'hardhat/types/providers';
 
